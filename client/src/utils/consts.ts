7d9ecfcccee99f6128355f37a007615e5ee704cc
Gilbert Zhang <gilbertzhang1208@gmail.com>
2022-12-30 19:47:12 -0500

    huh

--- client/src/utils/consts.ts
+++ client/src/utils/consts.ts
@@ -1,3 +1,5 @@
-// export const BACKEND_URL =
-//   process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
-export const BACKEND_URL = "https://radix-server.gilbertz.works";
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+// export const BACKEND_URL = "https://radix-server.gilbertz.works";
+// https://radix-server.gilbertz.works
+// https://radix-server.gilbertz.works
