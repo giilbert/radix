05e98e2f83c056db9937fcf77d8abf20930ba785
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-09-16 11:57:37 -0400

    feat: show test cases on problem view

--- client/src/components/problems/problem-view.tsx
+++ client/src/components/problems/problem-view.tsx
@@ -1,17 +1,36 @@
-import { Problem } from "@/types/problem";
-import { Heading, Tag, Text } from "@chakra-ui/react";
+import { Problem, TestCase } from "@/types/problem";
+import {
+  Box,
+  Code,
+  Divider,
+  HStack,
+  Heading,
+  Tag,
+  Text,
+} from "@chakra-ui/react";
 import { MarkdownRender } from "../ui/markdown-render";
 import { DifficultyTag } from "./difficulty-tag";
 
-export const ProblemView: React.FC<{ problem: Problem }> = ({ problem }) => {
+export const ProblemView: React.FC<{
+  problem: Omit<Problem, "defaultTestCases"> & { testCases: TestCase[] };
+}> = ({ problem }) => {
   return (
     <>
       <Heading mb="2">{problem.title}</Heading>
       <DifficultyTag difficulty={problem.difficulty} />
-
-      <hr />
-
+      <Divider my="2" />
       <MarkdownRender content={problem.description} />
+      <Divider my="2" />
+      <Heading mb="2" size="md" as="h2">
+        Test Cases
+      </Heading>
+      {problem.testCases.map((testCase, i) => (
+        <HStack key={i} mb="2">
+          <Code>
+            {testCase.input} =&gt; {testCase.output}
+          </Code>
+        </HStack>
+      ))}
     </>
   );
 };
